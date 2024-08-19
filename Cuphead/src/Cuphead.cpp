be5f27be15cb012bddbda29fe580ff0cfdaf828e
orange2458 <asm1133@naver.com>
2024-08-19 09:32:51 +0900

    GetMessage에서 PeekMessage로 메시지 처리 방식 변경

--- Cuphead/src/Cuphead.cpp
+++ Cuphead/src/Cuphead.cpp
@@ -42,13 +42,18 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 
     MSG msg;
 
-    // 기본 메시지 루프입니다:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
+    while ( true ) {
+        if ( PeekMessage( &msg, nullptr, 0, 0, PM_REMOVE ) ) {
+            if ( msg.message == WM_QUIT )
+                break;
+
+            if ( !TranslateAccelerator( msg.hwnd, hAccelTable, &msg ) ) {
+                TranslateMessage( &msg );
+                DispatchMessage( &msg );
+            }
+        }
+        else {
+
         }
     }
 
