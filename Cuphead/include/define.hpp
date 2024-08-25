d77bd3d818646a252d024ab6d9f847c18e29c56b
orange2458 <asm1133@naver.com>
2024-08-26 03:58:20 +0900

    키 입력 확인 매크로 함수

--- Cuphead/include/define.hpp
+++ Cuphead/include/define.hpp
@@ -19,6 +19,12 @@
 #define fDT Timer::GetInst().getFDT()
 #define DT Timer::GetInst().getDT()
 
+#define KEY_CHECK( key, state ) InputDeviceHandler::GetInst().getKeyState(key) == state
+#define KEY_TAP( key ) KEY_CHECK( key, KEY_STATE::TAP )
+#define KEY_HOLD( key ) KEY_CHECK( key, KEY_STATE::HOLD )
+#define KEY_AWAY( key ) KEY_CHECK( key, KEY_STATE::AWAY )
+#define KEY_NONE( key ) KEY_CHECK( key, KEY_STATE::NONE )
+
 enum class GROUP_TYPE
 {
 	DEFAULT,
