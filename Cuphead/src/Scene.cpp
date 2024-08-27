b949b98f3c2e44d34f3dcf5f7761e5e26c242a55
orange2458 <asm1133@naver.com>
2024-08-27 17:53:29 +0900

    Object들의 update 방식 변경
    std::for_each를 사용했을 경우에는 update 도중에 objList_가 수정되면 iterator가 invalid될 가능성이 있기 때문에 문제가 된다. 따라서 임시방편으로 복사된 objList_의 update를 진행한다.

--- Cuphead/src/Scene.cpp
+++ Cuphead/src/Scene.cpp
@@ -12,7 +12,7 @@ Scene::~Scene( )
 {
 	// Scene에 등록된 Object들을 delete
 	std::for_each( objList_.begin( ), objList_.end( ), []( auto& objs ) {
-		std::for_each( objs.begin( ), objs.end( ), []( auto& obj ) {
+		std::for_each( objs.begin( ), objs.end( ), []( auto obj ) {
 			delete obj;
 		} );
 	} );
@@ -21,18 +21,29 @@ Scene::~Scene( )
 void Scene::update( )
 {
 	// Scene에 등록된 Object들을 update
-	std::for_each( objList_.begin( ), objList_.end( ), []( auto& objs ) {
-		std::for_each( objs.begin( ), objs.end( ), []( auto& obj ) {
+	auto objListCopy = objList_;
+	std::for_each( objListCopy.begin( ), objListCopy.end( ), []( auto& objs ) {
+		std::for_each( objs.begin( ), objs.end( ), []( auto obj ) {
 			obj->update( );
 		} );
 	} );
+
+	/*for ( UINT i = 0; i < (UINT)GROUP_TYPE::EOE; ++i )
+	{
+		for ( size_t j = 0; j < objList_[ i ].size( ); ++j )
+		{
+
+			objList_[ i ][ j ]->update( );
+
+		}
+	}*/
 }
 
 void Scene::render( HDC hdc )
 {
 	// Scene에 등록된 Object들을 render
 	std::for_each( objList_.begin( ), objList_.end( ), [&hdc]( auto& objs ) {
-		std::for_each( objs.begin( ), objs.end( ), [&hdc]( auto& obj ) {
+		std::for_each( objs.begin( ), objs.end( ), [&hdc]( auto obj ) {
 			obj->render( hdc );
 		} );
 	} );
