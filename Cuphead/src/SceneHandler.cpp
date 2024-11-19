496b49b37eeea8daecf3a1e41b070ecfb86befc8
orange2458 <asm1133@naver.com>
2024-11-20 07:17:48 +0900

    추가 및 수정

--- Cuphead/src/SceneHandler.cpp
+++ Cuphead/src/SceneHandler.cpp
@@ -17,13 +17,14 @@ SceneHandler::~SceneHandler( ) {
 
 void SceneHandler::init( ) {
 	// Scene 생성
-	sceneList_[ static_cast<UINT>( SCENE_TYPE::TITLE_SCENE ) ] = new TitleScene{ };
+	sceneList_[ static_cast<UINT>( SCENE_TYPE::TITLE_SCENE ) ] = new TitleScene( );
 	sceneList_[ static_cast<UINT>( SCENE_TYPE::TITLE_SCENE ) ]->setSceneName( L"Title Scene" );
 
-	sceneList_[ static_cast<UINT>( SCENE_TYPE::MENU_SCENE ) ] = new MenuScene{ };
+	sceneList_[ static_cast<UINT>( SCENE_TYPE::MENU_SCENE ) ] = new MenuScene( );
 	sceneList_[ static_cast<UINT>( SCENE_TYPE::MENU_SCENE ) ]->setSceneName( L"Menu Scene" );
 
-	
+	sceneList_[ static_cast<UINT>( SCENE_TYPE::WORLD_SCENE ) ] = new WorldScene( );
+	sceneList_[ static_cast<UINT>( SCENE_TYPE::WORLD_SCENE ) ]->setSceneName( L"World Scene" );
 
 	// 현재 Scene 설정
 	currScene_ = sceneList_[ static_cast<UINT>( SCENE_TYPE::TITLE_SCENE ) ];
