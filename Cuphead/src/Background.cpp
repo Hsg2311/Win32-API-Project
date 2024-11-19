d1141ab9df648ddc80eccf9c724fe8e304aba9f5
orange2458 <asm1133@naver.com>
2024-11-19 23:46:32 +0900

    Background 이미지 그리는 방식 변경

--- Cuphead/src/Background.cpp
+++ Cuphead/src/Background.cpp
@@ -16,7 +16,7 @@ void Background::render( HDC hdc ) {
 	auto srcWidth = texture_->getWidth( );
 	auto srcHeight = texture_->getHeight( );
 
-	texture_->draw( hdc, xDest, yDest, destWidth, destHeight,
+	texture_->draw( hdc, xDest, yDest, srcWidth, srcHeight,
 					0, 0, srcWidth, srcHeight );
 
 	componentRender( hdc );
