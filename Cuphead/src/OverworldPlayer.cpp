53c42bd7fcefef3a0940ee2cf4b82a2fc7dc0b58
orange2458 <asm1133@naver.com>
2024-11-20 15:22:13 +0900

    파일 경로 수정

--- Cuphead/src/OverworldPlayer.cpp
+++ Cuphead/src/OverworldPlayer.cpp
@@ -71,65 +71,65 @@ void OverworldPlayer::update( ) {
 	}
 
 	if ( bUp && !bDown && !bLeft && !bRight ) {
-		getAnimator( )->play( L"Walk_Up" );
+		getAnimator( )->play( L"Cuphead_Walk_Up" );
 		objPos.y -= 300.f * fDT;
 	}
 	if ( bDown && !bUp && !bLeft && !bRight ) {
-		getAnimator( )->play( L"Walk_Down" );
+		getAnimator( )->play( L"Cuphead_Walk_Down" );
 		objPos.y += 300.f * fDT;
 	}
 	if ( bLeft && !bRight && !bUp && !bDown ) {
-		getAnimator( )->play( L"Walk_Left" );
+		getAnimator( )->play( L"Cuphead_Walk_Left" );
 		objPos.x -= 300.f * fDT;
 	}
 	if ( bRight && !bLeft && !bUp && !bDown ) {
-		getAnimator( )->play( L"Walk_Right" );
+		getAnimator( )->play( L"Cuphead_Walk_Right" );
 		objPos.x += 300.f * fDT;
 	}
 	if ( bLeft && bUp && !bDown && !bRight ) {
-		getAnimator( )->play( L"Walk_Left_Up" );
+		getAnimator( )->play( L"Cuphead_Walk_Left_Up" );
 		objPos.x -= 300.f * lu.x * fDT;
 		objPos.y -= 300.f * lu.y * fDT;
 	}
 	if ( bRight && bUp && !bDown && !bLeft ) {
-		getAnimator( )->play( L"Walk_Right_Up" );
+		getAnimator( )->play( L"Cuphead_Walk_Right_Up" );
 		objPos.x += 300.f * ru.x * fDT;
 		objPos.y -= 300.f * ru.y * fDT;
 	}
 	if ( bLeft && bDown && !bUp && !bRight ) {
-		getAnimator( )->play( L"Walk_Left_Down" );
+		getAnimator( )->play( L"Cuphead_Walk_Left_Down" );
 		objPos.x -= 300.f * ld.x * fDT;
 		objPos.y += 300.f * ld.y * fDT;
 	}
 	if ( bRight && bDown && !bUp && !bLeft ) {
-		getAnimator( )->play( L"Walk_Right_Down" );
+		getAnimator( )->play( L"Cuphead_Walk_Right_Down" );
 		objPos.x += 300.f * rd.x * fDT;
 		objPos.y += 300.f * rd.y * fDT;
 	}
 
 	if ( KEY_AWAY( InputData::UP ) ) {
-		getAnimator( )->play( L"Idle_Up" );
+		getAnimator( )->play( L"Cuphead_Idle_Up" );
 	}
 	if ( KEY_AWAY( InputData::DOWN ) ) {
-		getAnimator( )->play( L"Idle_Down" );
+		getAnimator( )->play( L"Cuphead_Idle_Down" );
 	}
 	if ( KEY_AWAY( InputData::LEFT ) ) {
-		getAnimator( )->play( L"Idle_Left" );
+		getAnimator( )->play( L"Cuphead_Idle_Left" );
 	}
 	if ( KEY_AWAY( InputData::RIGHT ) ) {
-		getAnimator( )->play( L"Idle_Right" );
+		getAnimator( )->play( L"Cuphead_Idle_Right" );
 	}
 	if ( KEY_AWAY( InputData::LEFT ) && KEY_AWAY( InputData::UP ) ) {
-		getAnimator( )->play( L"Idle_Left_Up" );
+		getAnimator( )->play( L"Cuphead_Idle_Left_Up" );
 	}
 	if ( KEY_AWAY( InputData::RIGHT ) && KEY_AWAY( InputData::UP ) ) {
-		getAnimator( )->play( L"Idle_Right_Up" );
+		getAnimator( )->play( L"Cuphead_Idle_Right_Up" );
 	}
 	if ( KEY_AWAY( InputData::LEFT ) && KEY_AWAY( InputData::DOWN ) ) {
-		getAnimator( )->play( L"Idle_Left_Down" );
+		getAnimator( )->play( L"Cuphead_Idle_Left_Down" );
 	}
 	if ( KEY_AWAY( InputData::RIGHT ) && KEY_AWAY( InputData::DOWN ) ) {
-		getAnimator( )->play( L"Idle_Right_Down" );
+		getAnimator( )->play( L"Cuphead_Idle_Right_Down" );
 	}
 
 	setObjPos( objPos );
--- Cuphead/src/WorldScene.cpp
+++ Cuphead/src/WorldScene.cpp
@@ -15,113 +15,113 @@ void WorldScene::entry( ) {
 
 	auto info2 = std::vector<texInfo>( );
 	info2.push_back( texInfo {
-		.resKey = L"Idle_Down",
-		.fileName = L"idle_down.png",
+		.resKey = L"Cuphead_Idle_Down",
+		.fileName = L"cuphead/idle_down.png",
 		.sliceSize = Vec2( 52.f, 96.f ),
 		.duration = 0.11f,
 		.frameCount = 5,
 		} );
 	info2.push_back( texInfo {
-		.resKey = L"Idle_Up",
-		.fileName = L"idle_up.png",
+		.resKey = L"Cuphead_Idle_Up",
+		.fileName = L"cuphead/idle_up.png",
 		.sliceSize = Vec2( 52.f, 82.f ),
 		.duration = 0.095f,
 		.frameCount = 6
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Idle_Left",
-		.fileName = L"idle_left.png",
+		.resKey = L"Cuphead_Idle_Left",
+		.fileName = L"cuphead/idle_left.png",
 		.sliceSize = Vec2( 62.f, 92.f ),
 		.duration = 0.11f,
 		.frameCount = 5
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Idle_Right",
-		.fileName = L"idle_right.png",
+		.resKey = L"Cuphead_Idle_Right",
+		.fileName = L"cuphead/idle_right.png",
 		.sliceSize = Vec2( 62.f, 92.f ),
 		.duration = 0.11f,
 		.frameCount = 5,
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Idle_Left_Up",
-		.fileName = L"idle_left_up.png",
+		.resKey = L"Cuphead_Idle_Left_Up",
+		.fileName = L"cuphead/idle_left_up.png",
 		.sliceSize = Vec2( 56.f, 84.f ),
 		.duration = 0.11f,
 		.frameCount = 5,
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Idle_Right_Up",
-		.fileName = L"idle_right_up.png",
+		.resKey = L"Cuphead_Idle_Right_Up",
+		.fileName = L"cuphead/idle_right_up.png",
 		.sliceSize = Vec2( 56.f, 84.f ),
 		.duration = 0.11f,
 		.frameCount = 5,
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Idle_Left_Down",
-		.fileName = L"idle_left_down.png",
+		.resKey = L"Cuphead_Idle_Left_Down",
+		.fileName = L"cuphead/idle_left_down.png",
 		.sliceSize = Vec2( 60.f, 110.f ),
 		.duration = 0.09f,
 		.frameCount = 7,
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Idle_Right_Down",
-		.fileName = L"idle_right_down.png",
+		.resKey = L"Cuphead_Idle_Right_Down",
+		.fileName = L"cuphead/idle_right_down.png",
 		.sliceSize = Vec2( 60.f, 110.f ),
 		.duration = 0.09f,
 		.frameCount = 7,
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Walk_Up",
-		.fileName = L"walk_up.png",
+		.resKey = L"Cuphead_Walk_Up",
+		.fileName = L"cuphead/walk_up.png",
 		.sliceSize = Vec2( 103.f, 114.f ),
 		.duration = 0.065f,
 		.frameCount = 12,
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Walk_Down",
-		.fileName = L"walk_down.png",
+		.resKey = L"Cuphead_Walk_Down",
+		.fileName = L"cuphead/walk_down.png",
 		.sliceSize = Vec2( 104.f, 110.f ),
 		.duration = 0.065f,
 		.frameCount = 13,
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Walk_Left",
-		.fileName = L"walk_left.png",
+		.resKey = L"Cuphead_Walk_Left",
+		.fileName = L"cuphead/walk_left.png",
 		.sliceSize = Vec2( 64.f, 90.f ),
 		.duration = 0.07f,
 		.frameCount = 11,
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Walk_Right",
-		.fileName = L"walk_right.png",
+		.resKey = L"Cuphead_Walk_Right",
+		.fileName = L"cuphead/walk_right.png",
 		.sliceSize = Vec2( 103.f, 110.f ),
 		.duration = 0.07f,
 		.frameCount = 11,
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Walk_Left_Up",
-		.fileName = L"walk_left_up.png",
+		.resKey = L"Cuphead_Walk_Left_Up",
+		.fileName = L"cuphead/walk_left_up.png",
 		.sliceSize = Vec2( 64.f, 94.f ),
 		.duration = 0.065f,
 		.frameCount = 12,
 		} );
 	info2.push_back( texInfo {
-		.resKey = L"Walk_Right_Up",
-		.fileName = L"walk_right_up.png",
+		.resKey = L"Cuphead_Walk_Right_Up",
+		.fileName = L"cuphead/walk_right_up.png",
 		.sliceSize = Vec2( 103.f, 114.f ),
 		.duration = 0.065f,
 		.frameCount = 12,
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Walk_Left_Down",
-		.fileName = L"walk_left_down.png",
+		.resKey = L"Cuphead_Walk_Left_Down",
+		.fileName = L"cuphead/walk_left_down.png",
 		.sliceSize = Vec2( 62.f, 102.f ),
 		.duration = 0.065f,
 		.frameCount = 12,
 	} );
 	info2.push_back( texInfo {
-		.resKey = L"Walk_Right_Down",
-		.fileName = L"walk_right_down.png",
+		.resKey = L"Cuphead_Walk_Right_Down",
+		.fileName = L"cuphead/walk_right_down.png",
 		.sliceSize = Vec2( 103.f, 110.f ),
 		.duration = 0.065f,
 		.frameCount = 12,
